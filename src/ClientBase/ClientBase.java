79eb29fa7a2af06973f36b17f8d07054bd20d2fc
parrarodriguez.manue <parrarodriguez.manue@TAVE-PC25.OSDBSESTO.local>
2021-02-18 12:53:19 +0100

    New client-server chat

---
+++ src/ClientBase/ClientBase.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ClientBase;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author parrarodriguez.manue
+ */
+public class ClientBase {
+    
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args){
+        System.out.println("Apertura connessione");
+        try {
+            while (true) {
+                Socket server = new Socket("127.0.0.1", 6666);
+                InputStream dalServer = server.getInputStream();
+                BufferedReader lettore = new BufferedReader(new InputStreamReader(dalServer));
+
+                String risposta = lettore.readLine();
+
+                System.out.println("Risposta del server : "+risposta);
+                lettore.close();
+                server.close();
+
+                System.out.println("Chiusura connessione !");
+                Thread.sleep(5000);
+            }
+        } catch (InterruptedException interrupt) {
+            Logger.getLogger(ClientBase.class.getName()).log(Level.SEVERE, null, interrupt);
+        } catch (IOException ex) {
+            Logger.getLogger(ClientBase.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
